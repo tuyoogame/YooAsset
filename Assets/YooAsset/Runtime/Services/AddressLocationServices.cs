99a3abf697f8793e6068733367a950f81db4e1af
hevinci <hevinci@hotmail.com>
2022-04-14 14:06:23 +0800

    Resource location interface extension

---
+++ Assets/YooAsset/Runtime/Services/AddressLocationServices.cs
@@ -0,0 +1,11 @@
+﻿
+namespace YooAsset
+{
+	public class AddressLocationServices : ILocationServices
+	{
+		public string ConvertLocationToAssetPath(YooAssets.EPlayMode playMode, string location)
+		{
+			throw new System.NotImplementedException("该功能暂未支持！");
+		}
+	}
+}
