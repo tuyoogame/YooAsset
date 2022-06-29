556b23b488735220e8228d7a0ade1715b9961106
hevinci <hevinci@hotmail.com>
2022-06-29 12:41:06 +0800

    Update OperationHandleBase.cs

--- Assets/YooAsset/Runtime/AssetSystem/Handles/OperationHandleBase.cs
+++ Assets/YooAsset/Runtime/AssetSystem/Handles/OperationHandleBase.cs
@@ -14,6 +14,15 @@ namespace YooAsset
 		}
 		internal abstract void InvokeCallback();
 
+
+		/// <summary>
+		/// 获取资源信息
+		/// </summary>
+		public AssetInfo GetAssetInfo()
+		{
+			return _assetInfo;
+		}
+
 		/// <summary>
 		/// 当前状态
 		/// </summary>
