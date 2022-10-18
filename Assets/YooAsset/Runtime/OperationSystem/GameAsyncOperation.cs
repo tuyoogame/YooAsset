97fc65e66da75dabba71b2e034bb217b9c633a73
hevinci <hevinci@hotmail.com>
2022-10-18 12:16:46 +0800

    Update OperationSystem
    增加新方法GameAsyncOperation.IsBusy()

--- Assets/YooAsset/Runtime/OperationSystem/GameAsyncOperation.cs
+++ Assets/YooAsset/Runtime/OperationSystem/GameAsyncOperation.cs
@@ -12,7 +12,22 @@ namespace YooAsset
 			OnUpdate();
 		}
 
+		/// <summary>
+		/// 异步操作开始
+		/// </summary>
 		protected abstract void OnStart();
+
+		/// <summary>
+		/// 异步操作更新
+		/// </summary>
 		protected abstract void OnUpdate();
+
+		/// <summary>
+		/// 异步操作系统是否繁忙
+		/// </summary>
+		protected bool IsBusy()
+		{
+			return OperationSystem.IsBusy;
+		}
 	}
 }
