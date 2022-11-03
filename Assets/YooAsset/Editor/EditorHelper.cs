1149906786546f1833dc6ec065e2ec4aeac6c399
hevinci <hevinci@hotmail.com>
2022-11-03 11:38:26 +0800

    Fix #29

--- Assets/YooAsset/Editor/EditorHelper.cs
+++ Assets/YooAsset/Editor/EditorHelper.cs
@@ -10,6 +10,7 @@ namespace YooAsset.Editor
 #if UNITY_2019_4_OR_NEWER
 		private readonly static Dictionary<System.Type, string> _uxmlDic = new Dictionary<System.Type, string>();
 
+		/*
 		static EditorHelper()
 		{
 			// 资源包收集
@@ -50,6 +51,46 @@ namespace YooAsset.Editor
 			{
 				throw new System.Exception($"Invalid YooAsset window type : {windowType}");
 			}
+		}
+		*/
+
+		/// <summary>
+		/// 加载窗口的布局文件
+		/// </summary>
+		public static UnityEngine.UIElements.VisualTreeAsset LoadWindowUXML<TWindow>() where TWindow : class
+		{
+			var windowType = typeof(TWindow);
+
+			// 缓存里查询并加载
+			if (_uxmlDic.TryGetValue(windowType, out string uxmlGUID))
+			{
+				string assetPath = AssetDatabase.GUIDToAssetPath(uxmlGUID);
+				if (string.IsNullOrEmpty(assetPath))
+				{
+					_uxmlDic.Clear();
+					throw new System.Exception($"Invalid UXML GUID : {uxmlGUID} ! Please close the window and open it again !");
+				}
+				var treeAsset = AssetDatabase.LoadAssetAtPath<UnityEngine.UIElements.VisualTreeAsset>(assetPath);
+				return treeAsset;
+			}
+
+			// 全局搜索并加载
+			string[] guids = AssetDatabase.FindAssets(windowType.Name);
+			if (guids.Length == 0)
+				throw new System.Exception($"Not found any assets : {windowType.Name}");
+
+			foreach (string assetGUID in guids)
+			{
+				string assetPath = AssetDatabase.GUIDToAssetPath(assetGUID);
+				var assetType = AssetDatabase.GetMainAssetTypeAtPath(assetPath);
+				if (assetType == typeof(UnityEngine.UIElements.VisualTreeAsset))
+				{
+					_uxmlDic.Add(windowType, assetGUID);
+					var treeAsset = AssetDatabase.LoadAssetAtPath<UnityEngine.UIElements.VisualTreeAsset>(assetPath);
+					return treeAsset;
+				}
+			}
+			throw new System.Exception($"Not found UXML file : {windowType.Name}");
 		}
 #endif
 
