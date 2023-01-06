c63e64691a26eb41cc61a4e385a36bd320873b30
Compa <1158261439@qq.com>
2023-01-06 20:16:43 +0800

    阅读源码

--- Assets/YooAsset/Samples~/Space Shooter/GameScript/Runtime/GameLogic/FsmNode/FsmInitGame.cs
+++ Assets/YooAsset/Samples~/Space Shooter/GameScript/Runtime/GameLogic/FsmNode/FsmInitGame.cs
@@ -30,10 +30,17 @@ internal class FsmInitGame : IStateNode
 	{
 		var handle = YooAssets.LoadAssetAsync<GameObject>("UICanvas");
 		//TODO wht ref 参考；同步接口LoadAssetSync；关闭Enable Addressable，使用全路径
-		//var handle = YooAssets.LoadAssetAsync<GameObject>("Assets/YooAsset/Assets/YooAsset/Samples/Space Shooter/GameRes/UIPanel/UICanvas.prefab");
-		
-		yield return handle;
-		var canvas = handle.InstantiateSync();		//TODO wht ref 参考；
+		// var handle = YooAssets.LoadAssetAsync<GameObject>("Assets/YooAsset/Assets/YooAsset/Samples/Space Shooter/GameRes/UIPanel/UICanvas.prefab");
+		handle.Completed += (AssetOperationHandle handle) => 
+		{
+			//TODO wht ref 加载回调
+		};
+		yield return handle;	//TODO wht ref 也可以用携程来确定加载是否完成
+		// handle.Release();	//TODO wht ref 释放资源		
+		// var package = YooAssets.GetAssetsPackage("DefaultPackage");
+    	// package.UnloadUnusedAssets();		//TODO wht ref 卸载引用计数为零的资源；切场景或按时间间隔来调
+
+		var canvas = handle.InstantiateSync();		//TODO wht ref 参考；同步实例化游戏对象
 		var desktop = canvas.transform.Find("Desktop").gameObject;
 		GameObject.DontDestroyOnLoad(canvas);
 
