1c2bbfea93b6057a6af7d995393230551937a087
hevinci <hevinci@hotmail.com>
2023-02-09 16:57:01 +0800

    update asset system
    修复了WEBGL平台加载原生文件失败的问题

--- Assets/YooAsset/Runtime/AssetSystem/AssetSystem.cs
+++ Assets/YooAsset/Runtime/AssetSystem/AssetSystem.cs
@@ -350,8 +350,11 @@ namespace YooAsset
 				return loader;
 
 			// 新增下载需求
-#if UNITY_WEBGL
-			loader = new AssetBundleWebLoader(this, bundleInfo);
+#if !UNITY_WEBGL
+			if (bundleInfo.Bundle.IsRawFile)
+				loader = new RawBundleFileLoader(this, bundleInfo);
+			else
+				loader = new AssetBundleWebLoader(this, bundleInfo);
 #else
 			if (bundleInfo.Bundle.IsRawFile)
 				loader = new RawBundleFileLoader(this, bundleInfo);
