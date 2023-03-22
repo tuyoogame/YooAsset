4820d2a54a775a206d583c59a55e653f2b01f482
hevinci <hevinci@hotmail.com>
2023-03-22 09:44:18 +0800

    update properties

--- Assets/YooAsset/Runtime/Properties/AssemblyInfo.cs
+++ Assets/YooAsset/Runtime/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("YooAsset.Editor")]
-[assembly: InternalsVisibleTo("YooAsset.EditorExtension")]
-[assembly: InternalsVisibleTo("YooAsset.RuntimeExtension")]
+[assembly: InternalsVisibleTo("Assembly-CSharp-Editor")]
