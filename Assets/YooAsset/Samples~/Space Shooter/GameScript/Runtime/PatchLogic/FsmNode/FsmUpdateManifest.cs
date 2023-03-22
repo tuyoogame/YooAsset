18830544a6189137fb9113c790d8ad2ef812627d
hevinci <hevinci@hotmail.com>
2023-03-22 18:58:46 +0800

    update samples

--- Assets/YooAsset/Samples~/Space Shooter/GameScript/Runtime/PatchLogic/FsmNode/FsmUpdateManifest.cs
+++ Assets/YooAsset/Samples~/Space Shooter/GameScript/Runtime/PatchLogic/FsmNode/FsmUpdateManifest.cs
@@ -38,6 +38,7 @@ public class FsmUpdateManifest : IStateNode
 
 		if(operation.Status == EOperationStatus.Succeed)
 		{
+			operation.SaveManifestVersion();
 			_machine.ChangeState<FsmCreateDownloader>();
 		}
 		else
