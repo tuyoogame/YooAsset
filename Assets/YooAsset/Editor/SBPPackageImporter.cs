77e8bef666e511374bba647be5a638926ace10a6
ALEXTANG <574809918@qq.com>
2024-04-09 15:30:08 +0800

    自动检测并导入Scriptablebuildpipeline

---
+++ Assets/YooAsset/Editor/SBPPackageImporter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace YooAsset.Editor
+{
+    /// <summary>
+    /// 自动导入SBP依赖类
+    /// </summary>
+    [InitializeOnLoad]
+    public static class SBPPackageImporter
+    {
+        static SBPPackageImporter()
+        {
+            InstallPackage("com.unity.scriptablebuildpipeline");
+        }
+        
+        private static bool IsPackageInstalled(string packageName)
+        {
+            return UnityEditor.PackageManager.PackageInfo.FindForAssetPath($"Packages/{packageName}") != null;
+        }
+
+        public static bool InstallPackage(string packageName)
+        {
+            if (IsPackageInstalled(packageName))
+            {
+                return false;
+            }
+            Debug.Log($"Install...{packageName}");
+            var request = Client.Add(packageName);
+            while (!request.IsCompleted) { };
+            if (request.Error != null) Debug.LogError(request.Error.message);
+            return request.Error == null;
+        }
+    }
+}
