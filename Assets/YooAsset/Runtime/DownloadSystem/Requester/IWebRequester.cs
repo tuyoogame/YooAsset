c9cc09cbed9d7f4f664e40f015ec13ae7673d8c3
hevinci <hevinci@hotmail.com>
2024-04-26 16:05:37 +0800

    fix #289

--- Assets/YooAsset/Runtime/DownloadSystem/Requester/IWebRequester.cs
+++ Assets/YooAsset/Runtime/DownloadSystem/Requester/IWebRequester.cs
@@ -14,52 +14,52 @@ namespace YooAsset
         /// <summary>
         /// 任务状态
         /// </summary>
-        public ERequestStatus Status { get; }
+        ERequestStatus Status { get; }
 
         /// <summary>
         /// 下载进度（0f~1f）
         /// </summary>
-        public float DownloadProgress { get; }
+        float DownloadProgress { get; }
 
         /// <summary>
         /// 已经下载的总字节数
         /// </summary>
-        public ulong DownloadedBytes { get; }
+        ulong DownloadedBytes { get; }
 
         /// <summary>
         /// 返回的网络错误
         /// </summary>
-        public string RequestNetError { get; }
+        string RequestNetError { get; }
 
         /// <summary>
         /// 返回的HTTP CODE
         /// </summary>
-        public long RequestHttpCode { get; }
+        long RequestHttpCode { get; }
 
 
         /// <summary>
         /// 创建任务
         /// </summary>
-        public void Create(string url, BundleInfo bundleInfo, params object[] args);
+        void Create(string url, BundleInfo bundleInfo, params object[] args);
 
         /// <summary>
         /// 更新任务
         /// </summary>
-        public void Update();
+        void Update();
 
         /// <summary>
         /// 终止任务
         /// </summary>
-        public void Abort();
+        void Abort();
 
         /// <summary>
         /// 是否已经完成（无论成功或失败）
         /// </summary>
-        public bool IsDone();
+        bool IsDone();
 
         /// <summary>
         /// 获取请求的对象
         /// </summary>
-        public object GetRequestObject();
+        object GetRequestObject();
     }
 }
