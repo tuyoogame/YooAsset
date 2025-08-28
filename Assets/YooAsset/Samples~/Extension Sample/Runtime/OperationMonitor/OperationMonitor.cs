0f73dc30474a29a040fbb345abfd5ae8e0cb6786
何冠峰 <hevinci@hotmail.com>
2025-08-28 22:08:02 +0800

    update sample

---
+++ Assets/YooAsset/Samples~/Extension Sample/Runtime/OperationMonitor/OperationMonitor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using YooAsset;
+
+public static class OperationMonitor
+{
+    public static void RegisterOperationCallback()
+    {
+        OperationSystem.RegisterStartCallback(OperationStartCallback);
+        OperationSystem.RegisterFinishCallback(OperationFinishCallback);
+    }
+
+    private static void OperationStartCallback(string packageName, AsyncOperationBase operation)
+    {
+        Debug.Log($"Operation start : {operation.GetType().Name}");
+    }
+    private static void OperationFinishCallback(string packageName, AsyncOperationBase operation)
+    {
+        Debug.Log($"Operation finish : {operation.GetType().Name}");
+    }
+}
